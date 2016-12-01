684df3e467fe306353ba372e8bd21462a7d2ac94
Brad Chao <brad@brad.tw>
2016-12-01 11:27:07 +0800

    v6

--- app/src/main/java/com/example/user/brad26/MainActivity.java
+++ app/src/main/java/com/example/user/brad26/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.user.brad26;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +16,15 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.LinkedList;
+
 public class MainActivity extends AppCompatActivity {
     private GridView gridView;
     private MyAdapter myAdapter;
@@ -22,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
     R.drawable.ball3,R.drawable.ball4,R.drawable.ball5,
     R.drawable.ball6,R.drawable.ball7,R.drawable.ball8,
     R.drawable.ball9,R.drawable.ball10};
+    private LinkedList<String> foodNo;
+    private UIHandler handler;
 
 
     @Override
@@ -29,8 +42,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        handler = new UIHandler();
         gridView = (GridView)findViewById(R.id.gridView);
-        initGridView();
+        //initGridView();
+
+        getJSON();
 
     }
 
@@ -47,13 +63,58 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void getJSON(){
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("http://data.fda.gov.tw/cacheData/19_3.json");
+                    HttpURLConnection conn =
+                            (HttpURLConnection) url.openConnection();
+                    conn.connect();
+                    BufferedReader reader =
+                            new BufferedReader(
+                                    new InputStreamReader(conn.getInputStream()));
+                    String line = reader.readLine();
+                    reader.close();
+
+                    parseJSON(line);
+                }catch(Exception e){
+                    Log.v("brad", e.toString());
+                }
+            }
+        }.start();
+    }
+
+    private void parseJSON(String json){
+        foodNo = new LinkedList<>();
+        try {
+            JSONArray root = new JSONArray(json);
+            for (int i=0; i<root.length(); i++){
+                JSONArray sub = root.getJSONArray(i);
+                JSONObject noObj = sub.getJSONObject(0);
+                String StringNo = noObj.getString("許可證字號");
+                Log.v("brad", StringNo);
+                foodNo.add(StringNo);
+            }
+            handler.sendEmptyMessage(0);
+        }catch(Exception e){
+            Log.v("brad", e.toString());
+        }
+
+    }
+
+    public void change(View v){
+        gridView.setNumColumns(3);
+    }
+
     private class MyAdapter extends BaseAdapter {
         private Context context;
         MyAdapter(Context context){this.context = context;}
 
         @Override
         public int getCount() {
-            return balls.length;
+            return 30;
         }
 
         @Override
@@ -76,7 +137,7 @@ public class MainActivity extends AppCompatActivity {
             }else{
                 img = (ImageView)view;
             }
-            img.setImageResource(balls[i]);
+            img.setImageResource(balls[(int)(Math.random()*10)]);
             return img;
         }
     }
@@ -103,15 +164,22 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
             if (view == null){
-                view = LayoutInflater.from(context).inflate(R.layout.layout_item,null);
+                LayoutInflater flater = LayoutInflater.from(context);
+                view = flater.inflate(R.layout.layout_item,null);
             }
             ImageView img = (ImageView)view.findViewById(R.id.item_img);
             TextView title = (TextView)view.findViewById(R.id.item_title);
 
-            img.setImageResource(balls[i]);
-            title.setText("Ball " +i);
-            if (i%2==0){
+            if (i == 7){
+                img.setImageResource(R.drawable.b0);
+            }else {
+                img.setImageResource(balls[(int)(Math.random()*10)]);
+            }
+            title.setText(foodNo.get(i));
+
+            if (i % 2 == 0){
                 view.setBackgroundColor(Color.YELLOW);
+                Log.v("brad", "i = " + i);
             }
 
 
@@ -119,7 +187,13 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
+    private class UIHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            initGridView();
+        }
+    }
 
 
 }
